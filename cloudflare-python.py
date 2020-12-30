a99e0c052f3e24f71218a21d766850572e4b267b
kasumiru <kasumiru>
2020-12-30 19:30:06 +0300

    fix syntax

--- cloudflare-python.py
+++ cloudflare-python.py
@@ -11,24 +11,24 @@ import json
 
 
 headers = {
-    'Content-Type':  'application/json'
+    'Content-Type': 'application/json'
 }
 
 
-with  open('creds.json') as e:
+with open('creds.json') as e:
     data = json.load(e)
 
-headers["X-Auth-Email"] = data["X-Auth-Email"]
-headers["X-Auth-Key"]   = data["X-Auth-Key"]
+headers['X-Auth-Email'] = data['X-Auth-Email']
+headers['X-Auth-Key']   = data['X-Auth-Key']
 
 
 
 def add(zone,record,ipaddr): 
     data = {
-        "type":     "A",
-        "ttl":      "120", 
-        "priority": 10, 
-        "proxied":  False
+        'type':     "A",
+        'ttl':      "120", 
+        'priority': 10, 
+        'proxied':  False
     }
     data['name']    = record
     data['content'] = ipaddr
@@ -41,31 +41,25 @@ def add(zone,record,ipaddr):
 
 def get_zone_id(zone):
     url = 'https://api.cloudflare.com/client/v4/zones'
-    r = requests.get(url, headers=headers,  json="")
-    for i in r.json()["result"]:
-        if i.get("name") == zone:
-            return(i.get("id"))
+    r = requests.get(url, headers=headers,  json='')
+    for i in r.json()['result']:
+        if i.get('name') == zone:
+            return i.get('id')
 
-
-def get_all_dns_records(zone):
-    url = 'https://api.cloudflare.com/client/v4/zones/{}/dns_records'.format(str(get_zone_id(zone)))
-    r = requests.get(url, headers=headers, json="")
-    return(r.json())
-
-
-def pr(n):
-    return(' ' * n)
 
 
 def get_a_dns_records(zone): 
     url = 'https://api.cloudflare.com/client/v4/zones/{}/dns_records'.format(str(get_zone_id(zone)))
-    r = requests.get(url, headers=headers, json="")
-
+    r = requests.get(url, headers=headers, json='')
+  
     list = []
-    for i in r.json().get("result"):
+    for i in r.json().get('result'):
         type = i.get('type')
-        if type == "A":
-            list.append(i.get("name"))
+        if type == 'A':
+            list.append(i.get('name'))
+
+    def print_space(n):
+        return ' ' * n
 
     def maxlen(list):
        x = 1
@@ -74,54 +68,51 @@ def get_a_dns_records(zone):
            if num > x:
                x = num
        x += 1
-       return(x)
+       return x 
 
-    for i in r.json().get("result"):
+    for i in r.json().get('result'):
         name = i.get('name')
         type = i.get('type')
         content = i.get('content')
-        if type == "A":
-            add = maxlen(list) - len(name)
-            interval = pr(add)
+        if type == 'A':
+            num = maxlen(list) - len(name)
+            interval = print_space(num)
             print('{0}{1}{2} {3}'.format(name,interval,type,content))
-    return(r.json())
-
+    return r.json()
 
 def get_dns_record_id(zone,record):
-    all_recs = get_all_dns_records(zone)
-    for i in all_recs.get("result"):
-        name = i["name"]
-        id   = i["id"]
+    url = 'https://api.cloudflare.com/client/v4/zones/{}/dns_records'.format(str(get_zone_id(zone)))
+    r = requests.get(url, headers=headers, json='')
+    all_records = r.json() 
+    for i in all_records.get('result'):
+        name = i['name']
+        id   = i['id']
         if name == record:
-            return(id)
+            return id
 
 def delete(record,zone):
-    print('now delete zone'.format(record))
+    print('now delete {}'.format(record))
     zone_id = str(get_zone_id(zone))
     dns_record_id = get_dns_record_id(zone,record)
-    url = "https://api.cloudflare.com/client/v4/zones/{}/dns_records/{}".format(zone_id,dns_record_id)
-    r = requests.delete(url, headers=headers, json="")
+    url = 'https://api.cloudflare.com/client/v4/zones/{}/dns_records/{}'.format(zone_id,dns_record_id)
+    r = requests.delete(url, headers=headers, json='')
     print(r.json())
-
-
 
 def get_all_zones_list():
     url = 'https://api.cloudflare.com/client/v4/zones'
-    r = requests.get(url, headers=headers, json="")
-    for i in r.json()["result"]:
-        print(i["name"])
-
+    r = requests.get(url, headers=headers, json='')
+    for i in r.json()['result']:
+        print(i['name'])
 
 def help():
     print('./cloudflare-python.py add       subdomain.yourDomain.com 8.8.8.8')
     print('./cloudflare-python.py del       subdomain.yourDomain.com ')
     print('./cloudflare-python.py get_all_a yourDomain.com ')
-    print('./cloudflare-python.py get_zonez')
+    print('./cloudflare-python.py get_zones')
     sys.exit(1)
 
-
 if len(sys.argv) == 2:
-   if sys.argv[1] == 'get_zonez':
+   if sys.argv[1] == 'get_zones':
        get_all_zones_list()
    else:
        help()
@@ -130,7 +121,7 @@ elif len(sys.argv) == 4:
     mode   = sys.argv[1]
     record = sys.argv[2]
     ipaddr = sys.argv[3]
-    zone   = ".".join(record.split(".")[-2:])
+    zone   = '.'.join(record.split('.')[-2:])
     if mode == 'add':
         add(zone,record,ipaddr)   
     else:
@@ -139,7 +130,7 @@ elif len(sys.argv) == 4:
 elif len(sys.argv) == 3:
     mode   = sys.argv[1]
     record = sys.argv[2]
-    zone   = ".".join(record.split(".")[-2:])
+    zone   = '.'.join(record.split('.')[-2:])
     if mode == 'del':
         delete(record,zone)
     elif mode == 'get_all_a':
@@ -148,4 +139,3 @@ elif len(sys.argv) == 3:
         help()
 else:
     help()
-
